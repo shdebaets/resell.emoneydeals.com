93300f0a1b55b6d8bedce9392e0cef75256e4e50
emoneyhq1 <emoneyhq1@gmail.com>
2025-10-23 21:57:16 -0400

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -10,7 +10,7 @@ import SuccessHeroSlider from "@/components/SuccessHeroSlider";
 import { useSearchParams, useRouter } from "next/navigation";
 import ScanOverlayPurchase from "@/components/ScanOverlayPurchase";
 
-const SIGNUP_URL = "https://whop.com/checkout/plan_jsROYnKAs4HoZ?d2c=true";
+const SIGNUP_URL = "https://whop.com/checkout/plan_5HqIumUyIij3D?d2c=true";
 
 type ApiResp = { items: any[]; count: number };
 
